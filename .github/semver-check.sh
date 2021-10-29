92dd0822a1f55f6480580c5beaa849c81490c409
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2021-10-29 14:22:54 -0300

    Add github step to avoid incorrect release versions

---
+++ .github/semver-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# This script checks that the version number of the release is an expected one, and avoid erroneous releases which don't follow semver
+set -eo pipefail
+
+VERSION="$1"
+PREV_VERSION=$(git describe --abbrev=0 --tags "v$1^")
+PREV_VERSION=${PREV_VERSION#v}
+PREV_MAJOR="${PREV_VERSION%%.*}"
+PREV_VERSION="${PREV_VERSION#*.}"
+PREV_MINOR="${PREV_VERSION%%.*}"
+PREV_PATCH="${PREV_VERSION#*.}"
+if [[ "$PREV_VERSION" == "$PREV_PATCH" ]]; then
+   PREV_PATCH="0"
+fi
+
+[[ "$VERSION" == "$PREV_MAJOR.$PREV_MINOR.$((PREV_PATCH + 1))" || "$VERSION" == "$PREV_MAJOR.$((PREV_MINOR + 1))" || "$VERSION" == "$((PREV_MAJOR + 1)).0" ]]
