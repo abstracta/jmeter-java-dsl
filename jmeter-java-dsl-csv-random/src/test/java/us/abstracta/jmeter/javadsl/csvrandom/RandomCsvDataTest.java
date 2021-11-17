02446578dc2ea266517fbe1ae95b0e5cf61d3364
Kirill Yurkov <kyurkov@samokat.ru>
2021-11-17 10:09:43 +0300

    Checkstyle autofix

--- jmeter-java-dsl-csv-random/src/test/java/us/abstracta/jmeter/javadsl/csvrandom/RandomCsvDataTest.java
+++ jmeter-java-dsl-csv-random/src/test/java/us/abstracta/jmeter/javadsl/csvrandom/RandomCsvDataTest.java
@@ -1,12 +1,6 @@
 package us.abstracta.jmeter.javadsl.csvrandom;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static us.abstracta.jmeter.javadsl.JmeterDsl.testPlan;
-import static us.abstracta.jmeter.javadsl.JmeterDsl.threadGroup;
-
-import static us.abstracta.jmeter.javadsl.csvrandom.RandomCsvDataSetConfig.csvRandomDataSet;
-import static us.abstracta.jmeter.javadsl.JmeterDsl.jsr223Sampler;
 import org.junit.jupiter.api.Test;
 import us.abstracta.jmeter.javadsl.JmeterDslTest;
 import us.abstracta.jmeter.javadsl.core.TestPlanStats;
@@ -14,25 +8,28 @@ import us.abstracta.jmeter.javadsl.core.TestPlanStats;
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
-import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+import static us.abstracta.jmeter.javadsl.csvrandom.RandomCsvDataSetConfig.csvRandomDataSet;
 
 public class RandomCsvDataTest extends JmeterDslTest {
 
-  @Test
-  public void shouldGetDataFromCSVDataSetRandom()
-      throws Exception {
-    URL res = getClass().getResource("/test_random.csv");
-    File file = Paths.get(res.toURI()).toFile();
-    String path = file.getAbsolutePath();
+    @Test
+    public void shouldGetDataFromCSVDataSetRandom()
+            throws Exception {
+        URL res = getClass().getResource("/test_random.csv");
+        File file = Paths.get(res.toURI()).toFile();
+        String path = file.getAbsolutePath();
 
-    System.out.println(path);
-    TestPlanStats stats = testPlan(
-        csvRandomDataSet(path).variableNames("test"),
-        threadGroup(1, 1,
-                jsr223Sampler("if (vars.get('test')!='foo') {SampleResult.setSuccessful(false); return 'FAIL'} else { return 'OK'}")
-            )
-    ).run();
-    assertThat(stats.overall().errorsCount()).isLessThan(1);
-  }
+        System.out.println(path);
+        TestPlanStats stats = testPlan(
+                csvRandomDataSet(path).variableNames("test"),
+                threadGroup(1, 1,
+                        jsr223Sampler("if (vars.get('test')!='foo') {SampleResult.setSuccessful(false); return 'FAIL'} else { return 'OK'}")
+                )
+        ).run();
+        assertThat(stats.overall().errorsCount()).isLessThan(1);
+    }
 
 }
