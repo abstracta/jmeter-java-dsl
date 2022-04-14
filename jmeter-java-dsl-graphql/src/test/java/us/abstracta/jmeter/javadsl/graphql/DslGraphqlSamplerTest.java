21a68ae03ba58efd470262ea05ba3c759217eac0
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2022-04-14 09:25:08 -0300

    Refactor graphqlSampler to move it to it's own module and only provide necessary methods instead of all http sampler ones
    Additionally, include tests, javadocs and user guide section

---
+++ jmeter-java-dsl-graphql/src/test/java/us/abstracta/jmeter/javadsl/graphql/DslGraphqlSamplerTest.java
@@ -0,0 +1,140 @@
+package us.abstracta.jmeter.javadsl.graphql;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static us.abstracta.jmeter.javadsl.JmeterDsl.testPlan;
+import static us.abstracta.jmeter.javadsl.JmeterDsl.threadGroup;
+import static us.abstracta.jmeter.javadsl.graphql.DslGraphqlSampler.graphqlSampler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.apache.http.entity.ContentType;
+import org.apache.jmeter.protocol.http.util.HTTPConstants;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import us.abstracta.jmeter.javadsl.JmeterDslTest;
+import us.abstracta.jmeter.javadsl.codegeneration.MethodCallBuilderTest;
+import us.abstracta.jmeter.javadsl.core.DslTestPlan;
+
+public class DslGraphqlSamplerTest extends JmeterDslTest {
+
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final String QUERY = "{ user(id: 1) { name } }";
+
+  @Test
+  public void shouldSendGraphqlQueryToServerWhenGraphqlSampler() throws Exception {
+    testPlan(
+        threadGroup(1, 1,
+            graphqlSampler(wiremockUri, QUERY)
+        )
+    ).run();
+    verify(postRequestedFor(anyUrl())
+        .withHeader(HTTPConstants.HEADER_CONTENT_TYPE,
+            equalTo(ContentType.APPLICATION_JSON.toString()))
+        .withRequestBody(equalToJson(buildRequestBody(null, null))));
+  }
+
+  @NotNull
+  private String buildRequestBody(String operationName, Map<String, Object> variables)
+      throws JsonProcessingException {
+    Map<String, Object> ret = new LinkedHashMap<>();
+    ret.put("operationName", operationName);
+    ret.put("query", QUERY);
+    if (variables != null) {
+      ret.put("variables", variables);
+    }
+    return OBJECT_MAPPER.writeValueAsString(ret);
+  }
+
+  @Test
+  public void shouldSendGraphqlQueryWithOperationWhenGraphqlSamplerWithOperation()
+      throws Exception {
+    String operationName = "myOp";
+    testPlan(
+        threadGroup(1, 1,
+            graphqlSampler(wiremockUri, QUERY)
+                .operationName(operationName)
+        )
+    ).run();
+    verify(postRequestedFor(anyUrl())
+        .withHeader(HTTPConstants.HEADER_CONTENT_TYPE,
+            equalTo(ContentType.APPLICATION_JSON.toString()))
+        .withRequestBody(equalToJson(buildRequestBody(operationName, null))));
+  }
+
+  @Test
+  public void shouldSendGraphqlQueryWithVariablesWhenGraphqlSamplerWithSimpleVariables()
+      throws Exception {
+    LinkedHashMap<String, Object> vars = new LinkedHashMap<>();
+    vars.put("var1", "val1");
+    vars.put("var2", 2);
+    vars.put("var3", 2.5);
+    vars.put("var4", false);
+    vars.put("var5", null);
+    DslGraphqlSampler sampler = graphqlSampler(wiremockUri, QUERY);
+    vars.forEach(sampler::variable);
+    testPlan(
+        threadGroup(1, 1,
+            sampler
+        )
+    ).run();
+    verify(postRequestedFor(anyUrl())
+        .withHeader(HTTPConstants.HEADER_CONTENT_TYPE,
+            equalTo(ContentType.APPLICATION_JSON.toString()))
+        .withRequestBody(equalToJson(buildRequestBody(null, vars))));
+  }
+
+  @Test
+  public void shouldSendGraphqlQueryWithVariablesWhenGraphqlSamplerWithRawVariables()
+      throws Exception {
+    LinkedHashMap<String, Object> vars = new LinkedHashMap<>();
+    vars.put("var1", 1);
+    vars.put("var2", "test");
+    vars.put("var3", Collections.singletonMap("prop", 3));
+    vars.put("var4", Collections.singletonList(1));
+    testPlan(
+        threadGroup(1, 1,
+            graphqlSampler(wiremockUri, QUERY)
+                .variables(OBJECT_MAPPER.writeValueAsString(vars))
+        )
+    ).run();
+    verify(postRequestedFor(anyUrl())
+        .withHeader(HTTPConstants.HEADER_CONTENT_TYPE,
+            equalTo(ContentType.APPLICATION_JSON.toString()))
+        .withRequestBody(equalToJson(buildRequestBody(null, vars))));
+  }
+
+  @Nested
+  public class CodeBuilderTest extends MethodCallBuilderTest {
+
+    public DslTestPlan testPlanWithSimpleGraphqlQuery() {
+      return testPlan(
+          threadGroup(1, 1,
+              graphqlSampler("http://localhost", "{ user(id: 1){ name } }")
+          )
+      );
+    }
+
+    public DslTestPlan testPlanWithGraphqlAndSimpleVariables() {
+      return testPlan(
+          threadGroup(1, 1,
+              graphqlSampler("http://localhost", "{ user(id: 1){ name } }")
+                  .operationName("myOp")
+                  .variable("var1", 1)
+                  .variable("var2", 2.5)
+                  .variable("var3", "test")
+                  .variable("var4", false)
+                  .variable("var5", null)
+          )
+      );
+    }
+
+  }
+}
