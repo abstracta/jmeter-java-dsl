16a3debc9723a182fe151a3e32f8a58242e3f521
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2022-09-15 13:40:34 -0300

    Improve ParallelControllerTest to be more robust and faster

--- jmeter-java-dsl-parallel/src/test/java/us/abstracta/jmeter/javadsl/parallel/ParallelControllerTest.java
+++ jmeter-java-dsl-parallel/src/test/java/us/abstracta/jmeter/javadsl/parallel/ParallelControllerTest.java
@@ -1,35 +1,33 @@
 package us.abstracta.jmeter.javadsl.parallel;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static org.assertj.core.api.Assertions.assertThat;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.httpSampler;
+import static us.abstracta.jmeter.javadsl.JmeterDsl.jsr223PostProcessor;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.testPlan;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.threadGroup;
 import static us.abstracta.jmeter.javadsl.parallel.ParallelController.parallelController;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import java.time.Duration;
+import java.util.concurrent.CyclicBarrier;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import us.abstracta.jmeter.javadsl.JmeterDslTest;
-import us.abstracta.jmeter.javadsl.core.TestPlanStats;
 
 public class ParallelControllerTest extends JmeterDslTest {
 
   @Test
+  @Timeout(30)
   public void shouldExecuteRequestsInParallelWhenRequestsInsideParallelController()
       throws Exception {
-    int responseTimeMillis = 3000;
-    stubFor(WireMock.any(WireMock.anyUrl())
-        .willReturn(WireMock.aResponse().withFixedDelay(responseTimeMillis)));
-    TestPlanStats stats = testPlan(
+    CyclicBarrier barrier = new CyclicBarrier(2);
+    testPlan(
         threadGroup(1, 1,
             parallelController(
-                httpSampler(wiremockUri),
                 httpSampler(wiremockUri)
+                    .children(jsr223PostProcessor(s -> barrier.await())),
+                httpSampler(wiremockUri)
+                    .children(jsr223PostProcessor(s -> barrier.await()))
             )
         )
     ).run();
-    assertThat(stats.duration()).isLessThan(Duration.ofMillis(responseTimeMillis * 2));
   }
 
 }
