65d59068019d0b40bdd69ae3fefe531169a6366b
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2023-03-07 11:52:59 -0300

    add cli config file schema to release artifacts

--- .github/collect-release-artifacts.sh
+++ .github/collect-release-artifacts.sh
@@ -4,4 +4,4 @@
 set -eo pipefail
 
 RET=$(find . -iname '*.jar' -and -not -iname '*-sources.jar' -and -not -iname '*-javadoc.jar' -and -not -iname '*-tests.jar' -and -not -iname 'original-*.jar' -and -not -iname '*-jmdsl-*.jar' | awk '{printf $0","}')
-echo "${RET%?}"
+echo "${RET%?},./jmeter-java-dsl-cli/src/main/json/jmdsl-config-schema.json"
