b1be04a8ebc366598d73f09a2a0d3d8272fd8b57
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2023-07-05 17:44:37 -0300

    Avoid adding dependency folder jars to release which are used just for integration tests

--- .github/collect-release-artifacts.sh
+++ .github/collect-release-artifacts.sh
@@ -3,5 +3,5 @@
 # This script solves artifacts to be uploaded to GH release
 set -eo pipefail
 
-RET=$(find . -iname '*.jar' -and -not -iname '*-sources.jar' -and -not -iname '*-javadoc.jar' -and -not -iname '*-tests.jar' -and -not -iname 'original-*.jar' -and -not -iname '*-jmdsl-*.jar' | awk '{printf $0","}')
+RET=$(find . -iname '*.jar' -and -not -iname '*-sources.jar' -and -not -iname '*-javadoc.jar' -and -not -iname '*-tests.jar' -and -not -iname 'original-*.jar' -and -not -iname '*-jmdsl-*.jar' -or -type d -name dependency -prune -and -not -iname 'dependency' | awk '{printf $0","}')
 echo "${RET%?},./jmeter-java-dsl-cli/src/main/json/jmdsl-config-schema.json"
